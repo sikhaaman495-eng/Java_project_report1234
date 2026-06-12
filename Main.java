57f038d87dc9a32089ae5ee5d25ec956aea3c3f4
sikhaaman495-eng <sikhaaman495@gmail.com>
2026-06-12 17:22:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ class Question {
     String[] options;
     String correctAnswer;
 
-    // Constructor
+    
+    // 1. Constructor used to initialize question details
+
     Question(String q, String[] opt, String ans) {
         questionText = q;
         options = opt;
@@ -16,8 +18,7 @@ class Question {
 class QuizManager {
     Question[] questions;
     int score = 0;
-
-    // Constructor
+// 2. Constructor creates and stores quiz questions
     QuizManager() {
         questions = new Question[2];
 
@@ -36,10 +37,10 @@ class QuizManager {
 
     void startQuiz() {
         Scanner sc = new Scanner(System.in);
-
+ // 3. Loop through all questions one by one
         for (int i = 0; i < questions.length; i++) {
             System.out.println("\nQ" + (i + 1) + ": " + questions[i].questionText);
-
+ // 4. Display all options for the current question
             for (int j = 0; j < questions[i].options.length; j++) {
                 System.out.println(questions[i].options[j]);
             }
@@ -59,7 +60,7 @@ class QuizManager {
         sc.close();
     }
 }
-
+// Main class
 public class Main {
     public static void main(String[] args) {
         QuizManager quiz = new QuizManager();
